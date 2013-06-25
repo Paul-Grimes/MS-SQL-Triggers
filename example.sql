f276a025ff42a1f867171d7e9c2fe67b0864ebae
Paul Grimes <paul.grimes@byprime.com>
2013-06-25 14:23:06 -0400

    Couple of additions here.
    1) Added some questions for me to explain.
    2) Showed simple insert trigger example.

--- example.sql
+++ example.sql
@@ -2,3 +2,20 @@
     This is an example script that will be used in a blog article I am writting for http://www.paul-grimes.com
 */
 
+-- MS SQL Specific
+-- -- What are the different types of triggers.
+-- -- When do you use CREATE vs ALTER
+-- -- How do you drop the trigger if it exisits
+-- -- What is inserted?
+-- -- What is deleted?
+
+-- Basic Syntax for an insert trigger
+CREATE TRIGGER prefix_TriggerName
+ON TABLENAME
+FOR INSERT
+AS
+BEGIN
+    UPDATE ANOTHERTABLE
+	SET value = (SELECT 1 FROM inserted WHERE name = 'Value')
+END
+GO
