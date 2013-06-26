7bc1c4811449a6bc7b3b160c68b2ca4b3fb5d45f
Paul Grimes <paul.grimes@byprime.com>
2013-06-26 10:36:00 -0400

    Added trigger removed some comments.
    The article is now posted

--- example.sql
+++ example.sql
@@ -2,15 +2,6 @@
     This is an example script that will be used in a blog article I am writting for http://www.paul-grimes.com
 */
 
--- MS SQL Specific
--- -- What are the different types of triggers.
--- -- When do you use CREATE vs ALTER
--- -- How do you drop the trigger if it exisits
--- -- What is inserted?
--- -- What is deleted?
-
--- How to check to see if a trigger exists and if so, delete it.
-
 -- This works for all triggers.
 IF EXISTS(
 	SELECT 
@@ -23,9 +14,8 @@ BEGIN
 END
 GO
 
--- This does not work for DDL Triggers
-
-IF EXISTS()
+-- This method does not work for DDL Triggers
+IF OBJECT_ID ('TriggerName', 'TR') IS NOT NULL
 BEGIN
 	DROP TRIGGER TriggerName;
 END
@@ -38,7 +28,19 @@ ON TABLENAME
 FOR INSERT
 AS
 BEGIN
-    UPDATE ANOTHERTABLE
-	SET value = (SELECT 1 FROM inserted WHERE name = 'Value')
+	SET NOCOUNT ON;
+
+	DECLARE @valueId INT;
+	SELECT @valueId = id FROM myTable WHERE name = 'Value';
+
+    IF EXISTS(SELECT 1 FROM inserted WHERE referenctId = @valueId)
+	BEGIN
+		EXEC msdb.dbo.sp_send_dbmail
+			@recipients = 'someone@example.com',
+			@profile_name = 'Defalut',
+			@subject = 'Your Subject',
+			@body ='Your body text'
+	END
 END
 GO
+  
